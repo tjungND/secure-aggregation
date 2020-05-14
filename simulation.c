92fc71a7e2fda18122979bec4c0a605d69c7643e
Taeho Jung <tjung@nd.edu>
2020-05-14 19:38:01 -0400

    Functionality implemented.

--- simulation.c
+++ simulation.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <gmp.h>
 #include <math.h>
+#include <gmp.h>
 
-#define TOP 10000
 #define E_FIXED_POINT 32
+#define MAX_USER_NUM 10
+#define MAX_DIMENSION 512
 
 
 void encode(mpz_t x, double y){
+    
     mpz_set_d(x, y * pow(2, E_FIXED_POINT));
+    
 }
 
 void decode(double *x, mpz_t y){
@@ -23,28 +26,97 @@ void decode(double *x, mpz_t y){
     mpf_clear(temp);
 }
 
+void simpleHash(mpz_t x, time_t y, mpz_t prime_base, mpz_t modulus){ // Our hash function does not need be cryptographic. All we need is a deterministic function that makes sure the base remains to be large (i.e., the x in H(t)^{p_nu}=x^{p_nu} has at least one large prime factor). This does not affect the security because the input of the hash is known to everyone.
+    // This function turns the UNIX time to a large integer deterministically by computing prime_base^y
+    
+    mpz_mul_ui(x, prime_base, y);
+    mpz_mod(x, x, modulus);
+}
 
 
-int main(){
+
+void generateKeys(mpz_t p_nu[], gmp_randstate_t state){
+    mpz_t s_nu_mu[MAX_USER_NUM][MAX_USER_NUM];
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        for(int j = 0; j < MAX_USER_NUM; j++){
+            mpz_init(s_nu_mu[i][j]);
+            if(i == j){
+                mpz_set_ui(s_nu_mu[i][j], 0);
+                continue;
+            }
+            mpz_urandomb(s_nu_mu[i][j], state, 1000);
+        }
+    }
+    
+    mpz_t p_nu_mu[MAX_USER_NUM][MAX_USER_NUM];
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        for(int j = 0; j < MAX_USER_NUM; j++){
+            mpz_init(p_nu_mu[i][j]);
+            mpz_sub(p_nu_mu[i][j], s_nu_mu[i][j], s_nu_mu[j][i]);
+        }
+    }
+    
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        mpz_init(p_nu[i]);
+        mpz_set_ui(p_nu[i], 0);
+        for(int j = 0; j < MAX_USER_NUM; j++){
+            mpz_clear(s_nu_mu[i][j]);
+            mpz_add(p_nu[i], p_nu[i], p_nu_mu[i][j]);
+            mpz_clear(p_nu_mu[i][j]);
+        }
+    }
+}
+
+
+void init3Darray(mpz_t ****arr){
+    *arr = (mpz_t***)malloc(MAX_USER_NUM * sizeof(mpz_t**));
+    //printf("%x\n", *arr);
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        (*arr)[i] = (mpz_t**)malloc(MAX_DIMENSION * sizeof(mpz_t*));
+        for(int j = 0; j < MAX_DIMENSION; j++){
+            (*arr)[i][j] = (mpz_t*)malloc(MAX_DIMENSION * sizeof(mpz_t));
+            for(int k = 0; k < MAX_DIMENSION; k++){
+                mpz_init((*arr)[i][j][k]);
+            }
+            
+        }
+    }
+}
+
+void clear3Darray(mpz_t ****arr){
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        for(int j = 0; j < MAX_DIMENSION; j++){
+            for(int k = 0; k < MAX_DIMENSION; k++){
+                mpz_clear((*arr)[i][j][k]);
+            }
+            free((*arr)[i][j]);
+        }
+        free((*arr)[i]);
+    }
+    free(*arr);
+}
+
+
+int main(int argc, char **argv){
     
     // This program uses a single machine to simulate multi-user secure aggregation
-
-	
-    int user_num[4] = {1, 2, 5, 10};
-    int dimension[5] = {32, 64, 128, 256, 512};
-    double user_gradient[4][512][512];
-
-	clock_t begin, end;
-	double time_spent;
-
-
-	mpz_t Random[TOP][10];
     
+
+    int user_num[4] = {1, 2, 5, MAX_USER_NUM};
+    int dimension[5] = {32, 64, 128, 256, MAX_DIMENSION};
     
     srand (time ( NULL));
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 512; j++){
-            for(int k = 0; k < 512; k++){
+    
+    double aggregate_gradient[MAX_DIMENSION][MAX_DIMENSION] = {0};
+    
+    //double user_gradient[MAX_USER_NUM][MAX_DIMENSION][MAX_DIMENSION];
+    double*** user_gradient;
+    user_gradient = (double***)malloc(MAX_USER_NUM * sizeof(double**));
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        user_gradient[i] = (double**)malloc(MAX_DIMENSION * sizeof(double*));
+        for(int j = 0; j < MAX_DIMENSION; j++){
+            user_gradient[i][j] = (double*)malloc(MAX_DIMENSION * sizeof(double));
+            for(int k = 0; k < MAX_DIMENSION; k++){
                 user_gradient[i][j][k] = (double)rand()/RAND_MAX*2.0-1.0;//float in range -1 to 1
             }
         }
@@ -56,141 +128,142 @@ int main(){
     gmp_randseed_ui(state, time(NULL));
     
     
-    mpz_t p,q,N,N2;
-    mpz_inits(p,q,N,N2,NULL);
-    mpz_randomb(p, state, 1024);
-    mpz_randomb(q, state, 1024);
+    mpz_t p,q,N,N2,p_inverse_mod_q,q_inverse_mod_p;
+    mpz_inits(p,q,N,N2,p_inverse_mod_q,q_inverse_mod_p,NULL);
+    mpz_urandomb(p, state, 1024);
+    mpz_nextprime(p, p);
+    mpz_urandomb(q, state, 1024);
+    mpz_nextprime(q, q);
+    mpz_invert(p_inverse_mod_q, p, q);
+    mpz_invert(q_inverse_mod_p, q, p);
     mpz_mul(N, p, q);
     mpz_mul(N2, N, N);
     
-    mpz_t s_nu_mu[10][10];
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            mpz_init(s_nu_mu[i][j]);
-            if(i == j){
-                mpz_set_ui(s_nu_mu[i][j], 0);
-                continue;
+    
+    mpz_t p_nu[MAX_USER_NUM];
+    generateKeys(p_nu, state);
+    
+    //mpz_t x_nu[MAX_USER_NUM][MAX_DIMENSION][MAX_DIMENSION];
+    mpz_t*** x_nu;
+    init3Darray(&x_nu);
+    
+    
+    //mpz_t y_nu[MAX_USER_NUM][MAX_DIMENSION][MAX_DIMENSION];
+    mpz_t*** y_nu;
+    init3Darray(&y_nu);
+    
+    
+    mpz_t prime_base; // this is the prime base for the hash implementation
+    mpz_init(prime_base);
+    mpz_urandomb(prime_base, state, 1024);
+    mpz_nextprime(prime_base, prime_base);
+    
+    
+    
+    // the following portion simulates the individual users who generate y_nu=(1+N)^{[x_nu]}H(t)^{p_nu} mod N^2
+    time_t current_time = time(NULL);
+    mpz_t temp;
+    mpz_init(temp);
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        for(int j = 0; j < MAX_DIMENSION; j++){
+            for(int k = 0; k < MAX_DIMENSION; k++){
+                printf("%d,%d,%d\n", i, j, k);
+                encode(x_nu[i][j][k], user_gradient[i][j][k]);
+                mpz_mul(temp, N, x_nu[i][j][k]);
+                mpz_add_ui(y_nu[i][j][k], temp, 1);
+                // y_nu[i][j][k] = (1+N)^{x_nu[i][j][k]}
+                simpleHash(temp, current_time, prime_base, N); // temp = H(t)
+                //mpz_powm(temp, temp, p_nu[i], N); // temp = H(t)^{p_nu[i]} mod N^2
+                mpz_mul(y_nu[i][j][k], y_nu[i][j][k], temp);
+                mpz_mod(y_nu[i][j][k], y_nu[i][j][k], N2); // y_nu[i][j][k] = final ciphertext to be broadcast
+                
             }
-            mpz_randomb(s_nu_mu[i][j], state, 2000);
-        }
-    }
-    
-    mpz_t p_nu_mu[10][10];
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            mpz_init(p_nu_mu[i][j]);
-            mpz_sub(p_nu_mu[i][j], s_nu_mu[i][j], s_nu_mu[j][i]);
-        }
-    }
-    mpz_t p_nu[10];
-    for(int i = 0; i < 10; i++){
-        mpz_init(p_nu[i]);
-        mpz_set_ui(p_nu[i], 0);
-        for(int j = 0; j < 10; j++){
-            mpz_clear(s_nu_mu[i][j]);
-            mpz_add(p_nu[i], p_nu_mu[i][j]);
-            mpz_clear(p_nu_mu[i][j]);
         }
     }
     
     
+    // the following portion simulates the aggregation at the parameter server side, who computes sum of user_gradients for each weight.
     
+    clock_t begin, end;
+    double time_spent;
     
+    begin = clock();
+    for(int j = 0; j< MAX_DIMENSION; j++){
+        for(int k = 0; k < MAX_DIMENSION; k++){
+            //printf("%d,%d\n", j, k);
+            for(int i = 0; i < MAX_USER_NUM; i++){
+                mpz_set_ui(temp, 1);
+                mpz_mul(temp, temp, y_nu[i][j][k]);
+            }
+            mpz_mod(temp, temp, N2);
+            mpz_sub_ui(temp, temp, 1);
+            mpz_tdiv_q(temp, temp, N);
+            decode(&(aggregate_gradient[j][k]), temp);
+            
+        }
+    }
+    end = clock();
+    
+    time_spent = (double)(end - begin) / (CLOCKS_PER_SEC);
 
-	
-	
+    printf("Total time: %f\n", time_spent * 1000);
+    
+    mpz_clear(temp);
+
+    return 0;
 
 
-	int i, j, n, repeat, k, prod;
 
-
-
-	mpz_t temp;
-	mpz_init(temp);
-
-	n = 10000;
-	
-	repeat = 100;
     
     FILE *f;
-	f = fopen("micro_enc_dec_run_time.csv", "w");
+	f = fopen("simulation.csv", "w");
 	fprintf(f, "num,user1,user2,others,aggregator\n");
 
+    //clock_t begin, end;
+    //double time_spent;
 
-
-	for(i = 1; i <= 100; i++){
+	for(int i = 1; i <= 100; i++){
 	
 		begin = clock();
 
-		for(k = 0; k < repeat; k++){
-
-			mpz_set_ui(temp, 1);
-	
-			for(j = 0; j < n -2; j++){
-				mpz_mul(temp, temp, small);
-				mpz_mod(temp, temp, small);
-			}
-			mpz_powm(temp, small, temp, large);
-
-		}
+		
 
 		end = clock();
 
 
-		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC * repeat);
+		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC);
 
 		fprintf(f, "%d,%f,", i, time_spent * 1000);
 
 		begin = clock();
-		for(k = 0; k < repeat; k++){
-
-
-			mpz_powm(temp, small, small, large);
-	
-			mpz_mul(temp, temp, small);
-			mpz_mod(temp, temp, large);
-
-		}
+		
+        
 
 		end = clock();
 
-		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC * repeat);
+		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC);
 
 		
 		fprintf(f, "%f,", time_spent * 1000);
 
 		begin = clock();
-		for(k = 0; k < repeat + 560; k++){
-
-
-			mpz_powm_ui(temp, small, i, small);
-			mpz_powm(temp, temp, small, small);
-			mpz_mul(temp, temp, small);
-			mpz_mod(temp, temp, small);
-
-
-		}
+		
+        
 
 		end = clock();
-		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC * (repeat + 560));
+		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC);
 
 		fprintf(f, "%f,", time_spent * 1000);
 		
 		
 		begin = clock();
 		
-		for(k = 0; k < repeat + 150; k++){
-			mpz_set_ui(temp, 1);
-			for(j = 0; j < n-1; j++){
-				mpz_mul(temp, temp, large);
-				mpz_mod(temp, temp, large);
-			}
-		}
 		
+        
 		
 		end = clock();
 		
-		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC * (repeat + 150));
+		time_spent = (double)(end - begin) / (CLOCKS_PER_SEC);
 		
 		fprintf(f, "%f\n", time_spent * 1000);
 
@@ -202,15 +275,27 @@ int main(){
 
 	
 
-	for(i = 0; i < TOP; i++){
-		for(j = 0; j < 10; j++){
-			mpz_clear(Random[i][j]);
-		}
-	}
-
 	gmp_randclear(state);
  
+    mpz_clears(p,q,N,N2,p_inverse_mod_q,q_inverse_mod_p,NULL);
+    mpz_clear(prime_base);
+    
+    for(int i = 0; i < MAX_USER_NUM; i++){
+        for(int j = 0; j < MAX_DIMENSION; j++){
+            free(user_gradient[i][j]);
+        }
+        free(user_gradient[i]);
+    }
+    free(user_gradient);
+    
+    
+    
+    clear3Darray(&x_nu);
+    clear3Darray(&y_nu);
+
+    
     return 0;
+ 
 }
 
 
---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <gmp.h>
+
+#define E_FIXED_POINT 32
+
+
+
+
+
+int main(int argc, char **argv){
+    
+    // This program uses a single machine to simulate multi-user secure aggregation
+    return 0;
+
+}
+
+
